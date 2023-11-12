bb7c659ab17ba13f5de4d752f3782944bf0d894a
David Baker <dpbaker1298@gmail.com>
2023-11-12 13:47:14 -0500

    Add tests for Run function in scan package

--- scan/scanHosts_test.go
+++ scan/scanHosts_test.go
@@ -1,6 +1,8 @@
 package scan_test
 
 import (
+	"net"
+	"strconv"
 	"testing"
 
 	"github.com/Dbaker1298/pScan/scan"
@@ -17,5 +19,78 @@ func TestStateString(t *testing.T) {
 
 	if ps.Open.String() != "open" {
 		t.Errorf("Expected %q, got %q instead\n", "open", ps.Open.String())
+	}
+}
+
+func TestRunHostFound(t *testing.T) {
+	testCases := []struct {
+		name          string
+		expectedState string
+	}{
+		{"OpenPort", "open"},
+		{"ClosedPort", "closed"},
+	}
+
+	// Testing against localhost
+	host := "localhost"
+	hl := scan.NewHostsList()
+
+	hl.Add(host)
+
+	ports := []int{}
+
+	// Init ports, 1 open, 1 closed
+	for _, tc := range testCases {
+		ln, err := net.Listen("tcp", net.JoinHostPort(host, "0"))
+		if err != nil {
+			t.Fatalf("Failed to listen on port: %v\n", err)
+		}
+
+		defer ln.Close()
+
+		_, portStr, err := net.SplitHostPort(ln.Addr().String())
+		if err != nil {
+			t.Fatalf("Failed to split host and port: %v\n", err)
+		}
+
+		port, err := strconv.Atoi(portStr)
+		if err != nil {
+			t.Fatalf("Failed to convert port string to int: %v\n", err)
+		}
+
+		ports = append(ports, port)
+
+		if tc.name == "ClosedPort" {
+			ln.Close()
+		}
+	}
+
+	res := scan.Run(hl, ports)
+
+	// Verify the results of HostFound test
+	if len(res) != 1 {
+		t.Fatalf("Expected 1 result, got %d instead\n", len(res))
+	}
+
+	if res[0].Host != host {
+		t.Errorf("Expected host %q, got %q instead\n", host, res[0].Host)
+	}
+
+	if res[0].NotFound {
+		t.Errorf("Expected host %q to be found, but it was not\n", host)
+	}
+
+	if len(res[0].PortStates) != 2 {
+		t.Fatalf("Expected 2 port states, got %d instead\n", len(res[0].PortStates))
+	}
+
+	for i, tc := range testCases {
+		if res[0].PortStates[i].Port != ports[i] {
+			t.Errorf("Expected port %d, got %d instead\n", ports[0], res[0].PortStates[i].Port)
+		}
+
+		if res[0].PortStates[i].Open.String() != tc.expectedState {
+			t.Errorf("Expected port %d to be %s\n", ports[i], tc.expectedState)
+		}
 	}
 }
