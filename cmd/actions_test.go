c21338bd1d517d9aa2b03eac027adf79ec655382
David Baker <dpbaker1298@gmail.com>
2023-11-12 12:45:50 -0500

    Add host actions test cases. Following the table-driven testing design pattern. Because our Actions are using external functions, and not the boilerplate from Cobra, we can more easily test the Actions. Add, List, and Delete.

--- cmd/actions_test.go
+++ cmd/actions_test.go
@@ -1,6 +1,8 @@
 package cmd
 
 import (
+	"bytes"
+	"io"
 	"io/ioutil"
 	"os"
 	"testing"
@@ -36,5 +38,62 @@ func setup(t *testing.T, hosts []string, initList bool) (string, func()) {
 	// Return the temporary file name and a function to clean up.
 	return tf.Name(), func() {
 		os.Remove(tf.Name())
+	}
+}
+
+func TestHostActions(t *testing.T) {
+	// Define hosts for actions test
+	hosts := []string{"host1", "host2", "host3"}
+
+	// Define test cases
+	testCases := []struct {
+		name           string
+		args           []string
+		expectedOut    string
+		initList       bool
+		actionFunction func(io.Writer, string, []string) error
+	}{
+		{
+			name:           "AddAction",
+			args:           hosts,
+			expectedOut:    "Added host: host1\nAdded host: host2\nAdded host: host3\n",
+			initList:       false,
+			actionFunction: addAction,
+		},
+		{
+			name:           "ListAction",
+			expectedOut:    "host1\nhost2\nhost3\n",
+			initList:       true,
+			actionFunction: listAction,
+		},
+		{
+			name:           "DeleteAction",
+			args:           []string{"host1", "host2"},
+			expectedOut:    "Deleted host: host1\nDeleted host: host2\n",
+			initList:       true,
+			actionFunction: deleteAction,
+		},
+	}
+
+	// Run test cases
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			// Set up Action test
+			tf, cleanup := setup(t, hosts, tc.initList)
+			defer cleanup()
+
+			// Define var to capture Action output
+			var out bytes.Buffer
+
+			// Execute Action and capture output
+			if err := tc.actionFunction(&out, tf, tc.args); err != nil {
+				t.Fatalf("Expected no error, got: %q\n", err)
+			}
+
+			// Compare output
+			if out.String() != tc.expectedOut {
+				t.Errorf("Expected output: %q, got: %q instead\n", tc.expectedOut, out.String())
+			}
+		})
 	}
 }
