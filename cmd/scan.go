b0b2f1ea254e154299dd0c44e08153be2f653302
David Baker <dpbaker1298@gmail.com>
2023-11-12 14:14:05 -0500

    Add scan functionality to pScan with updated hosts list

--- cmd/scan.go
+++ cmd/scan.go
@@ -17,8 +17,11 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 package cmd
 
 import (
+	"fmt"
+	"io"
 	"os"
 
+	"github.com/Dbaker1298/pScan/scan"
 	"github.com/spf13/cobra"
 )
 
@@ -39,6 +42,42 @@ var scanCmd = &cobra.Command{
 
 		return scanAction(os.Stdout, hostsFile, ports)
 	},
+}
+
+func scanAction(out io.Writer, hostsFile string, ports []int) error {
+	hl := &scan.HostsList{}
+
+	if err := hl.Load(hostsFile); err != nil {
+		return err
+	}
+
+	results := scan.Run(hl, ports)
+
+	return printResults(out, results)
+}
+
+func printResults(out io.Writer, results []scan.Results) error {
+	message := ""
+
+	for _, r := range results {
+		message += fmt.Sprintf("%s:", r.Host)
+
+		if r.NotFound {
+			message += " Host not found\n\n"
+			continue
+		}
+
+		message += fmt.Sprintln()
+
+		for _, p := range r.PortStates {
+			message += fmt.Sprintf("\t%d: %s\n", p.Port, p.Open)
+		}
+
+		message += fmt.Sprintln()
+	}
+
+	_, err := fmt.Fprint(out, message)
+	return err
 }
 
 func init() {
